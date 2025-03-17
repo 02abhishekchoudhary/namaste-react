422f5c7a1725e0570c8ed3591e0901f7ff7b511d
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2025-03-17 21:29:49 +0530

    05- React hook(useState), VDOM, Render, Diff Algo, React Fiber

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,28 @@
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 import resList from "../utils/mockData";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
   return (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            console.log("Button Clicked");
+            const filteredList = resList.filter(
+              (res) => res.data.avgRating > 4
+            );
+            console.log(filteredList);
+            setListOfRestaurants(filteredList);
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {resList.map((restaurant, index) => (
+        {listOfRestaurants.map((restaurant, index) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
