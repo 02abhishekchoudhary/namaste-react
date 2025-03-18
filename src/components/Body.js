99dcfd135485e123c46718eb00e59622a116bd14
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2025-03-18 23:27:23 +0530

    06- useEffect, monolith vs Microservices, Swiggy API Fetch

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,24 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
+// import resList from "../utils/mockData";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6139298&lng=77.2088282&collection=83633&tags=layout_CCS_NorthIndian&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
+    );
+    const json = await data.json();
+    console.log(json);
+    console.log(json.data.cards[3].card.card);
+    // console.log(json?.data?.cards[4]?.card?.card?.info);
+    // setListOfRestaurants(json?.data?.cards[4]?.card?.card?.info);
+  };
 
   return (
     <div className="body">
@@ -14,13 +29,13 @@ const Body = () => {
             const filteredList = resList.filter(
               (res) => res.data.avgRating > 4
             );
-            console.log(filteredList);
+            // console.log(filteredList);
             setListOfRestaurants(filteredList);
           }}
         >
           Top Rated Restaurants
         </button>
-        <button
+        {/* <button
           className="filter-btn"
           onClick={() => {
             const sortedList = resList.sort(
@@ -31,7 +46,7 @@ const Body = () => {
           }}
         >
           Sort By Rating
-        </button>
+        </button> */}
       </div>
       <div className="res-container">
         {listOfRestaurants.map((restaurant, index) => (
