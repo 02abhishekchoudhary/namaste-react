87dd8856e12b3421589e939459b8d8543ba94488
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2025-03-19 21:45:57 +0530

    06- Swiggy API issue resolved

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,5 @@
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
-// import resList from "../utils/mockData";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
@@ -11,13 +10,14 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6139298&lng=77.2088282&collection=83633&tags=layout_CCS_NorthIndian&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6139298&lng=77.2088282&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-    console.log(json);
-    console.log(json.data.cards[3].card.card);
-    // console.log(json?.data?.cards[4]?.card?.card?.info);
-    // setListOfRestaurants(json?.data?.cards[4]?.card?.card?.info);
+    const restaurantData = json?.data?.cards?.find((item) =>
+      item?.card?.card?.id?.includes("restaurant_grid")
+    )?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+    console.log(restaurantData);
+    setListOfRestaurants(restaurantData);
   };
 
   return (
@@ -26,8 +26,8 @@ const Body = () => {
         <button
           className="filter-btn"
           onClick={() => {
-            const filteredList = resList.filter(
-              (res) => res.data.avgRating > 4
+            const filteredList = listOfRestaurants.filter(
+              (res) => res?.resData?.avgRating > 4
             );
             // console.log(filteredList);
             setListOfRestaurants(filteredList);
@@ -50,7 +50,10 @@ const Body = () => {
       </div>
       <div className="res-container">
         {listOfRestaurants.map((restaurant, index) => (
-          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+          <RestaurantCard
+            key={restaurant?.info?.id}
+            resData={restaurant?.info}
+          />
         ))}
       </div>
     </div>
