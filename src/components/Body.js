dbda9905d1f094dfd5c18a0b46b8a668305c969d
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2025-03-21 20:45:16 +0530

    06.1.2- Delivery time, CORS fixed

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,7 +13,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6139298&lng=77.2088282&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6139298&lng=77.2088282&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     const restaurantData = json?.data?.cards?.find((item) =>
@@ -35,7 +35,6 @@ const Body = () => {
             value={searchText}
             onChange={(e) => setSearchText(e.target.value)}
           />
-
           <button
             onClick={() => {
               const filteredRestaurants = listOfRestaurants.filter((res) =>
@@ -47,27 +46,25 @@ const Body = () => {
             Search
           </button>
         </div>
-
         <button
           className="filter-btn"
           onClick={() => {
             const filteredList = listOfRestaurants.filter(
               (res) => res?.info?.avgRating > 4.3
             );
-            console.log(filteredList);
+            // console.log(filteredList);
             setFilteredRestaurants(filteredList);
           }}
         >
           Top Rated Restaurants
         </button>
-
         <button
           className="filter-btn"
           onClick={() => {
             const sortedList = [...listOfRestaurants].sort(
               (a, b) => a?.info?.avgRating - b?.info?.avgRating
             );
-            console.log(sortedList);
+            // console.log(sortedList);
             setFilteredRestaurants(sortedList);
           }}
         >
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,8 +1,9 @@
 import { CDN_URL } from "../utils/constants";
 
 const RestaurantCard = (props) => {
-  const { cloudinaryImageId, name, avgRating, cuisines, costForTwo } =
-    props?.resData;
+  const { resData } = props;
+  const { cloudinaryImageId, name, avgRating, cuisines, costForTwo, sla } =
+    resData;
 
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
@@ -13,8 +14,9 @@ const RestaurantCard = (props) => {
       />
       <h3>{name}</h3>
       <h4>{cuisines?.join(", ")}</h4>
-      <h4>{avgRating} stars</h4>
-      <h4>{costForTwo}</h4>
+      <h4>Rating: {avgRating} stars</h4>
+      <h4>Price: {costForTwo}</h4>
+      <h4>Delivery In: {sla?.slaString}</h4>
     </div>
   );
 };
