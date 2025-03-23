3a999283a57b45a4105af597fd217845b7ca3d1b
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2025-03-23 23:45:52 +0530

    07- Children routes, Link

--- src/components/Body.js
+++ src/components/Body.js
@@ -15,8 +15,11 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
+    // const data = await fetch(
+    //   "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6139298&lng=77.2088282&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    // );
     const data = await fetch(
-      "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6139298&lng=77.2088282&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6139298&lng=77.2088282&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     const restaurantData = json?.data?.cards?.find((item) =>
