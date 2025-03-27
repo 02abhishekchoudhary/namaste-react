4b1106e4cdd259001fd26ffed0ac90c2c76e69c6
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2025-03-28 00:36:21 +0530

    08- State in CBC

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 const User = (props) => {
   const { name, location } = props;
+  const [count] = useState(0);
+  const [count2] = useState(1);
   return (
     <div className="user-card">
+      <h2>Count: {count}</h2>
+      <h2>Count: {count2}</h2>
       <h3>{name}</h3>
       <h4>{location}</h4>
       <h5>Contact: @narutouzumaki</h5>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,11 +3,19 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 0,
+      count2: 1,
+    };
   }
   render() {
     const { name, location } = this.props;
+    const { count, count2 } = this.state;
     return (
       <div className="user-card">
+        <h2>Count: {count}</h2>
+        <h2>Count: {count2}</h2>
         <h3>{name}</h3>
         <h4>{location}</h4>
         <h5>Contact: @itachiuchia</h5>
