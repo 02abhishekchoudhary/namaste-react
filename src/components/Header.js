59d69ffc0ac7ff611413732739cdc4edc59d1792
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2025-04-02 22:57:15 +0530

    10- Tailwinfd CSS classes

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,30 +9,32 @@ const Header = () => {
   const onlineStatus = useOnlineStatus();
   // console.log("Body Render");
   return (
-    <div className="header">
+    <div className="flex justify-between">
       <div className="logo-container">
-        <img src={LOGO_URL} alt="Logo of food delivery app" className="logo" />
+        <img src={LOGO_URL} alt="Logo of food delivery app" className="w-56" />
       </div>
-      <div className="nav-items">
-        <ul>
-          <li>Online Status: {onlineStatus ? "🟢" : "🔴"}</li>
-          <li>
+      <div className="flex items-center">
+        <ul className="flex p-4 m-4">
+          <li className="px-4 text-lg">
+            Online Status: {onlineStatus ? "🟢" : "🔴"}
+          </li>
+          <li className="px-4">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/about">About Us</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/grocery">Grocery</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/contact">Contact Us</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link>Cart</Link>
           </li>
           <button
-            className="login"
+            className="border border-black px-4 bg-green-200"
             onClick={() =>
               btnNameReact === "Login"
                 ? setBtnNamereact("Logout")
