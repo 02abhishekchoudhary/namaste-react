b5357f772f87f0f312f300d2af494786be7ba7e0
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2025-04-13 20:33:36 +0530

    13- Unit testing(Basic), test syntex, it, describe, coverage

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,16 +2,21 @@ const Contact = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold text-center pt-4">Contact Us</h1>
-      {/* <h2 className="text-xl font-bold text-center pt-4">
+      <p className="text-xl font-bold text-center pt-4">
         Welcome to contact us page of food delivery app
-      </h2> */}
+      </p>
       <form className="text-center pt-8">
         <label className="font-semibold">Name: </label>
-        <input type="text" className="border border-black p-2 m-2 rounded-sm" />
+        <input
+          type="text"
+          className="border border-black p-2 m-2 rounded-sm"
+          placeholder="Itachi Uchia"
+        />
         <label className="font-semibold">Email: </label>
         <input
           type="email"
           className="border border-black p-2 m-2 rounded-sm"
+          placeholder="itachiuchia@mail.com"
         />
         <button className="border border-black bg-slate-400 rounded-lg p-2 m-2">
           Submit
--- src/components/__tests__/contact.test.js
+++ src/components/__tests__/contact.test.js
@@ -2,10 +2,35 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
-test("Should load contact us component", () => {
-  render(<Contact />);
+describe("Contact Us Page Test Case", () => {
+  test("Should load contact us component", () => {
+    render(<Contact />);
+    const heading = screen.getByRole("heading");
+    // Assertion
+    expect(heading).toBeInTheDocument();
+  });
 
-  const heading = screen.getByRole("heading");
+  test("Should load Button insidde contact component", () => {
+    render(<Contact />);
+    const button = screen.getByRole("button");
+    // const button = screen.getByText("Random");
+    // Assertion
+    expect(button).toBeInTheDocument();
+  });
 
-  expect(heading).toBeInTheDocument();
+  test("Should load input name inside contact component", () => {
+    render(<Contact />);
+    const input = screen.getByPlaceholderText("Itachi Uchia");
+    // Assertion
+    expect(input).toBeInTheDocument();
+  });
+
+  it("Should load two input boxes on the Contact component", () => {
+    render(<Contact />);
+    const inputBoxes = screen.getAllByRole("textbox");
+    console.log(inputBoxes.length);
+    // Assertion
+    // expect(inputBoxes.length).not.toBe(3);
+    expect(inputBoxes.length).toBe(2);
+  });
 });
